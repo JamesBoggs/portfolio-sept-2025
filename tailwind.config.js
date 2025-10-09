023f2d47018ad1dbbafab96653b0f81a0d8a7e80
JamesBoggs <166553741+JamesBoggs@users.noreply.github.com>
2025-10-09 11:15:45 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
-theme: {
-  extend: {
-    colors: {
-      tiffany: '#81D8D0',
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        tiffany: "#81D8D0",
+      },
+      boxShadow: {
+        card: "0 10px 20px rgba(0,0,0,0.25)",
+      },
     },
   },
-},
+  plugins: [],
+};
